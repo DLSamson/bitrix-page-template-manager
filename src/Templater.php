7b88ec40432e0df9c0a7b97b66185df2df419b2b
DLSamson <DLSamson@yandex.ru>
2025-11-25 22:21:36 +0300

    change args names. add comments to 'hard to think' sections. small refactoring

--- src/Templater.php
+++ src/Templater.php
@@ -14,8 +14,7 @@ class Templater
 
     /**
      * @param string $templateDir   pathToFolder with all templates
-     * @param mixed $varsToPass     variables to pass into template
-     * @throws \InvalidArgumentException    config
+     * @param mixed $globalVariablesToPass     variables to pass into template
      */
     public function __construct(string $templateDir, array $globalVariablesToPass = []) {
         $this->templateDir = str_ends_with($templateDir, '/') // If possible, we use polyfill
@@ -28,18 +27,26 @@ class Templater
     protected string $templateDir;
     protected array $globalVariablesToPass = [];
 
+    /**
+     * @throws TemplateFileNotFoundException
+     * @throws BadMethodCallException
+     */
     public function __call($name, $arguments) {
         $regex = '/load(?<type>.*?)Template/';
-        
+
+        // Expect load{Type}Template method pattern
         if (!preg_match($regex, $name, $matches))
             throw new BadMethodCallException(sprintf('Method does not exsits or does not match pattern \'load{Type}Template\', found: %s', $name));
 
+        // Too many args
         if (count($arguments) > 2)
             throw new BadMethodCallException(sprintf('Expected exactly 0, 1 or 2 arguments, got %s', count($arguments)));
 
+        // If first argument is presented, it should be a name
         if(isset($arguments[0]) && gettype($arguments[0]) !== 'string')
             throw new BadMethodCallException(sprintf('Expected argument 1 to be type "string", got %s', gettype($arguments[0])));
 
+        // if second argument is presented, it should be values
         if(isset($arguments[1]) && gettype($arguments[1]) !== 'array')
             throw new BadMethodCallException(sprintf('Expected argument 2 to be type "array", got %s', gettype($arguments[1])));
 
@@ -49,25 +56,29 @@ class Templater
 
         $this->loadTemplate($name, $type, $values);
     }
-    
-    public function loadTemplate(string $name, string $type, array $values = []) : void { // @TODO add tests with values
+
+    /**
+     * @throws TemplateFileNotFoundException
+     */
+    public function loadTemplate(string $name, string $type, array $values = []) : void {
 
         $pathToTemplateFile = $name 
-            ? sprintf('%s.%s.php', $this->parseName($name), $type)
-            : sprintf('%s%s.php', $this->parseName($name), $type);
+            ? sprintf('%s.%s.php', $this->getPathToFile($name), $type) // loadTypeTemplate($name <-- provided)
+            : sprintf('%s%s.php', $this->getPathToFile($name), $type); // loadTypeTemplate($name <-- NOT provided or empty)
 
+        // In case name was not provided, it will be something like `pathToTemplateDir/type.php`
         if (!file_exists($pathToTemplateFile))
             throw new TemplateFileNotFoundException(sprintf('Template file with name %s and type %s not found at %s', $name ?: '*empty*', $type, $pathToTemplateFile));
 
         $this->includeTemplateFile($pathToTemplateFile, $values);
     }
 
-    protected function parseName(string $name) : string {
+    protected function getPathToFile(string $name) : string {
         $pathParts = explode('.', $name);
         return $this->templateDir . '/' . implode('/', $pathParts);
     }
     
-    protected function includeTemplateFile(string $file, array $values = []) {
+    protected function includeTemplateFile(string $file, array $values = []): void {
         global $APPLICATION, $USER, $DB;
 
         (function (string $file, array $varsToPass, array $values) use ($APPLICATION, $USER, $DB) {
